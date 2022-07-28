8b705df0d86888d55062d5c3900fbea9470ddc81
soroushfathi <81929775+soroushfathi@users.noreply.github.com>
2022-07-28 19:03:45 +0430

    change quantity from "PositiveIntegerField" to "IntegerField", because we Øthe decrease button for product added

--- cart/models.py
+++ cart/models.py
@@ -7,7 +7,7 @@ class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE, blank=True, null=True)
-    quantity = models.PositiveIntegerField()
+    quantity = models.IntegerField()
 
     def __str__(self):
         return self.user.username
