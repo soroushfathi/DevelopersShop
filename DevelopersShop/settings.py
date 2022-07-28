2a1daac570e9b0615dc8498f8104ca20e1371b41
soroushfathi <81929775+soroushfathi@users.noreply.github.com>
2022-07-28 19:14:33 +0430

    1. add static urls for each app 2.config new apps

--- DevelopersShop/settings.py
+++ DevelopersShop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'account.apps.AccountConfig',
+    'cart.apps.CartConfig',
+    'order.apps.OrderConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    ('cart', os.path.join(BASE_DIR, 'cart', 'static')),
+]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 CKEDITOR_UPLOAD_PATH = 'ck'
--- DevelopersShop/urls.py
+++ DevelopersShop/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('home.urls', namespace='home')),
     path('account/', include('account.urls', namespace='account')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('cart/', include('cart.urls', namespace='cart')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +\
               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
