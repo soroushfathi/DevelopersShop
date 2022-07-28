50fdfbf360a99bc8b9b72b7d639f6f30f1df6601
soroushfathi <81929775+soroushfathi@users.noreply.github.com>
2022-07-28 19:09:15 +0430

    1.remove cart func 2.improve add cart func(add decrease button) 3. complete view of carts page

--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,14 @@ from django.contrib import messages
 
 
 def cart_detail(request):
-    return render(request, 'cart/cart.html')
+    carts = Cart.objects.filter(user_id=request.user.id)
+    total = 0
+    for cart in carts:
+        if cart.product.status != 'None':
+            total += cart.variant.total_price * cart.quantity
+        else:
+            total += cart.product.total_price * cart.quantity
+    return render(request, 'cart/cart.html', {'carts': carts, 'total': total})
 
 
 def add_cart(request, pid):
@@ -18,7 +25,7 @@ def add_cart(request, pid):
         - if cart has not variant, we have to find product by product id
     '''
     if product.status != 'None':
-        var_id = request.POST.get('select')
+        var_id = request.POST.get('varid')
         cart = Cart.objects.filter(user_id=request.user.id, variant_id=var_id)
         if cart:
             # same product(with same variant) have been added, so we increase quantity
@@ -40,16 +47,33 @@ def add_cart(request, pid):
             if flag:
                 if product.status != 'None':
                     getcart = Cart.objects.get(user_id=request.user.id, product_id=pid, variant_id=var_id)
+                    if getcart.variant.amount >= getcart.quantity + quan > 0:
+                        getcart.quantity += quan
+                    elif getcart.quantity + quan > getcart.variant.amount:
+                        messages.error(request, 'درخواست بیش از حد موجودی است', 'danger')
+                    elif getcart.quantity + quan <= 0:
+                        messages.error(request, 'کمتر از یک عدد نامعتبر', 'danger')
                 else:
-                    getcart = Cart.objects.get(user_id=request.user.id, product_id=pid)
-                getcart.quantity += quan
+                    if getcart.product.amount >= getcart.quantity + quan > 0:
+                        getcart.quantity += quan
+                    elif getcart.quantity + quan > getcart.product.amount:
+                        messages.error(request, 'درخواست بیش از حد موجودی است', 'danger')
+                    elif getcart.product + quan <= 0:
+                        messages.error(request, 'کمتر از یک عدد نامعتبر', 'danger')
                 getcart.save()
             else:
                 Cart.objects.create(product_id=pid, user_id=request.user.id, variant_id=var_id, quantity=quan)
+                messages.success(request, '{} تعداد از این محصول با موفقیت به سبد خرید اضافه شد'.format(quan), 'success')
         return redirect(url)
     else:
         messages.error(request, 'دست نکن بچه :)', 'danger')
 
 
-def remove_cart(request):
+def add_cart_plus(request, pid, amount):
     pass
+
+
+def remove_cart(request, cid):
+    url = request.META.get('HTTP_REFERER')
+    Cart.objects.filter(id=cid).delete()
+    return redirect(url)
