a4821f15bfb72d01f2d947bb4025f3566cfee576
soroushfathi <81929775+soroushfathi@users.noreply.github.com>
2022-08-01 09:55:27 +0430

    add Order & ItemOrder to admin panel

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Order, ItemOrder
 
-# Register your models here.
+
+class ItemOrderInline(admin.TabularInline):
+    model = ItemOrder
+    readonly_fields = ['user', 'product', 'variant', 'quantity']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'email', 'last_name', 'address', 'create', 'paid']
+    inlines = [ItemOrderInline]
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(ItemOrder)
