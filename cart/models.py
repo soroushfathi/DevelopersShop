90566dcb7d307ae36ca8dbbcfefb16bb618b846e
soroushfathi <soroush8fathi@gmail.com>
2022-09-04 00:40:48 +0430

    new Class: Compare

--- cart/models.py
+++ cart/models.py
@@ -12,8 +12,27 @@ class Cart(models.Model):
     def __str__(self):
         return self.user.username
 
+    class Meta:
+        verbose_name = 'سبد خرید'
+        verbose_name_plural = 'سبد خرید ها'
+
 
 class CartForm(ModelForm):
     class Meta:
         model = Cart
         fields = ['quantity']
+
+
+class Compare(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    sessionkey = models.CharField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return self.product.name
+
+
+class CompareForm(ModelForm):
+    class Meta:
+        model = Compare
+        fields = ['product']
