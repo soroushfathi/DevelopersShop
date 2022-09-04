ef7291e77524881a4d09488b053ad7c4298fbf0e
soroushfathi <soroush8fathi@gmail.com>
2022-09-04 12:27:06 +0430

    debug + def show_compare

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'cart'
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
-    path('add-cart/<int:pid>', views.add_cart, name='add_cart')
+    path('add-cart/<int:pid>/', views.add_cart, name='add_cart'),
+    path('remove/<int:cid>/', views.remove_cart, name='remove_cart'),
+    path('compare/<int:pid>/', views.compare, name='compare'),
+    path('show-compare/', views.show_compare, name='show_compare'),
 ]
