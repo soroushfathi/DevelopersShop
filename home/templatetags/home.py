98af6f7cc1e8ad1488cd78d69c93626c1d05fc6f
soroushfathi <soroush8fathi@gmail.com>
2022-09-09 01:12:22 +0430

    create simple tempalte tag for filter url

--- home/templatetags/home.py
+++ home/templatetags/home.py
@@ -1,3 +1,14 @@
 from django import template
 
 register = template.Library
+
+
+@register.simple_tag()
+def filter_url(number, name, urlencode=None):
+    url = f'?{name}={number}'
+    if urlencode:
+        sp_query = urlencode.split('&')
+        sp_filter = filter(lambda x: x.split('=')[0] != name, sp_query)
+        join_query = '&'.join(sp_filter)
+        url = f'{url}&{join_query}'
+    return url
