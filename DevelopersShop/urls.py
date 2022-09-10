9b31874a8a7785346e636f6bda0dfcf07da87214
soroushfathi <soroush8fathi@gmail.com>
2022-09-10 10:34:35 +0430

    add sitemaps path

--- DevelopersShop/urls.py
+++ DevelopersShop/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from home.sitemaps import ProductViewSitemap
+
+sitemaps = {
+    'product': ProductViewSitemap,
+}
 
 urlpatterns = [
     path('whatdoyouwant/', admin.site.urls),
@@ -25,5 +31,6 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('order/', include('order.urls', namespace='order')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +\
               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
