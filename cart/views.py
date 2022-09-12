51b71630c0f76f36cd2f8c1a18a0ba8ce1c3a196
soroushfathi <soroush8fathi@gmail.com>
2022-09-12 23:04:34 +0430

    rewrite functions: add\show Cart for not authenticated user

--- cart/views.py
+++ cart/views.py
@@ -4,29 +4,36 @@ from .models import Cart, CartForm, Compare
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from order.forms import OrderForm
+from django.views.decorators.http import require_POST
 
 
 def cart_detail(request):
-    carts = Cart.objects.filter(user_id=request.user.id)
-    user = request.user
-    prof = request.user.profile
     total = 0
-    for cart in carts:
-        if cart.product.status != 'None':
-            total += cart.variant.total_price * cart.quantity
-        else:
-            total += cart.product.total_price * cart.quantity
-    orderform = OrderForm()
-    context = {
-        'carts': carts,
-        'total': total,
-        'orderform': orderform,
-        'user': user, 'prof': prof,
-    }
-    return render(request, 'cart/cart.html', context=context)
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user_id=request.user.id)
+        user = request.user
+        prof = request.user.profile
+        for cart in carts:
+            if cart.product.status != 'None':
+                total += cart.variant.total_price * cart.quantity
+            else:
+                total += cart.product.total_price * cart.quantity
+        orderform = OrderForm()
+        context = {'carts': carts, 'total': total, 'orderform': orderform, 'user': user, 'prof': prof}
+        return render(request, 'cart/cart.html', context=context)
+    else:
+        carts = Cart.objects.filter(user=None, sessionkey=request.session.session_key)
+        if carts.exists():
+            for cart in carts:
+                if cart.product.status != 'None':
+                    total += cart.variant.total_price * cart.quantity
+                else:
+                    total += cart.product.total_price * cart.quantity
+        context = {'carts': carts, 'total': total}
+        return render(request, 'cart/cart.html', context=context)
 
 
-@login_required(login_url='account:login')
+@require_POST
 def add_cart(request, pid):
     url = request.META.get('HTTP_REFERER')
     product = Product.objects.get(id=pid)
@@ -36,53 +43,48 @@ def add_cart(request, pid):
         - if cart has variant, we have to find product by variant id
         - if cart has not variant, we have to find product by product id
     '''
+    is_auth = request.user.is_authenticated
+    session_key = request.session.session_key
+    user_id = request.user.id
+    # if cart exist: same product(with same variant) have been added, so we increase quantity
     if product.status != 'None':
         var_id = request.POST.get('varid')
-        cart = Cart.objects.filter(user_id=request.user.id, variant_id=var_id)
-        if cart:
-            # same product(with same variant) have been added, so we increase quantity
-            flag = True
-        else:
-            flag = False
+        cart = Cart.objects.filter(user_id=user_id if is_auth else None, variant_id=var_id, sessionkey=session_key if not is_auth else None)
+        flag = True if cart.exists() else False
     else:
-        cart = Cart.objects.filter(user_id=request.user.id, product_id=pid)
-        if cart:
-            # same product have been added, so we increase quantity
-            flag = True
-        else:
-            flag = False
+        cart = Cart.objects.filter(user_id=user_id if is_auth else None, product_id=pid, sessionkey=session_key if not is_auth else None)
+        flag = True if cart.exists() else False
 
-    if request.method == 'POST':
-        form = CartForm(request.POST)
-        if form.is_valid():
-            quan = form.cleaned_data['quantity']
-            if flag:
-                if product.status != 'None':
-                    getcart = Cart.objects.get(user_id=request.user.id, product_id=pid, variant_id=var_id)
-                    if getcart.variant.amount >= getcart.quantity + quan > 0:
-                        getcart.quantity += quan
-                    elif getcart.quantity + quan > getcart.variant.amount:
-                        messages.error(request, 'درخواست بیش از حد موجودی است', 'danger')
-                    elif getcart.quantity + quan <= 0:
-                        messages.error(request, 'کمتر از یک عدد نامعتبر', 'danger')
-                else:
-                    getcart = Cart.objects.get(user_id=request.user.id, product_id=pid)
-                    if getcart.product.amount >= getcart.quantity + quan > 0:
-                        getcart.quantity += quan
-                    elif getcart.quantity + quan > getcart.product.amount:
-                        messages.error(request, 'درخواست بیش از حد موجودی است', 'danger')
-                    elif getcart.product + quan <= 0:
-                        messages.error(request, 'حداقل تعداد باید یکی باشد', 'danger')
-                getcart.save()
+    form = CartForm(request.POST)
+    if form.is_valid():
+        quan = form.cleaned_data['quantity']
+        if flag:
+            if product.status != 'None':
+                getcart = Cart.objects.get(user_id=user_id if is_auth else None, product_id=pid, variant_id=var_id, sessionkey=session_key if not is_auth else None)
+                if getcart.variant.amount >= getcart.quantity + quan > 0:
+                    getcart.quantity += quan
+                elif getcart.quantity + quan > getcart.variant.amount:
+                    messages.error(request, 'درخواست بیش از حد موجودی است.', 'danger')
+                elif getcart.quantity + quan <= 0:
+                    messages.error(request, 'تعداد محصول نمیتواند کمتر از 1 باشد.', 'danger')
             else:
+                getcart = Cart.objects.get(user_id=user_id if is_auth else None, product_id=pid, sessionkey=session_key if not is_auth else None)
+                if getcart.product.amount >= getcart.quantity + quan > 0:
+                    getcart.quantity += quan
+                elif getcart.quantity + quan > getcart.product.amount:
+                    messages.error(request, 'درخواست بیش از حد موجودی است.', 'danger')
+                elif getcart.product + quan <= 0:
+                    messages.error(request, 'تعداد محصول نمیتواند کمتر از 1 باشد.', 'danger')
+            getcart.save()
+        else:
+            if is_auth:
                 Cart.objects.create(product_id=pid, user_id=request.user.id, variant_id=var_id, quantity=quan)
-                messages.success(request, '{} تعداد از این محصول با موفقیت به سبد خرید اضافه شد'.format(quan), 'success')
-        return redirect(url)
-    else:
-        messages.error(request, 'دست نکن بچه :)', 'danger')
+            else:
+                Cart.objects.create(user=None, product_id=pid, variant_id=var_id, quantity=quan)
+            messages.success(request, 'تعداد {} تا از این محصول با موفقیت به سبد خرید اضافه شد'.format(quan), 'success')
+    return redirect(url)
 
 
-@login_required(login_url='account:login')
 def remove_cart(request, cid):
     url = request.META.get('HTTP_REFERER')
     Cart.objects.filter(id=cid).delete()
