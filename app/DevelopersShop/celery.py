55629dc646720919236cf89fff628eb6a969fd03
soroushfathi <soroush8fathi@gmail.com>
2023-02-10 14:14:27 +0330

    set celery config

--- app/DevelopersShop/celery.py
+++ app/DevelopersShop/celery.py
@@ -3,15 +3,10 @@ from celery import Celery
 
 PROJ_NAME = 'DevelopersShop'
 
-os.environ.setdefault('DJANGO_SETTING_MODULE', f'{PROJ_NAME}.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{PROJ_NAME}.settings')
 
 app = Celery(PROJ_NAME)
 
-app.config_from_object('django.conf:setting', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+# app.autodiscover_tasks()
